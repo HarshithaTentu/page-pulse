3eb1ce169ce04cd6224b04f33be374ff2f5f0767
HarshithaTentu <harshithatentu09@gmail.com>
2026-07-25 01:30:02 +0530

    Add CORS support for frontend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.schemas import AuditRequest
 from app.services import audit_url
-from app.middleware import request_logger
 
 
 app = FastAPI(
@@ -12,14 +12,22 @@ app = FastAPI(
 )
 
 
-# Add request logging middleware
-app.middleware("http")(request_logger)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5500",
+        "https://page-pulse.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
     return {
-        "message": "Page Pulse API is running 🚀"
+        "message": "Page Pulse API is running"
     }
 
 
